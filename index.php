1b3f77202dd6d76a68c536ccc946c3b0aa805a91
Santiago <santiagopujol7@gmail.com>
2020-12-02 16:54:08 +0000

    Comienzo del projecto

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CRUD_TASK</title>
+</head>
+<body>
+	<?php include("db.php"); ?>
+</body>
+</html>
