c9779e1475dfe8117882d9cf7b19ac6a94245d40
Santiago <santiagopujol7@gmail.com>
2020-12-03 22:47:09 +0000

    estilos del boton agregados

--- index.php
+++ index.php
@@ -2,4 +2,23 @@
 <?php include("includes/header.php"); ?>
 
 
+<div class="container p-4">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="card card-body">
+				<form action="">
+					<div class="form-group">
+						<input type="text" name="title" class="form-control" placeholder="Task Title">
+						<div class="form-group">
+							<textarea class="form-control" name="descriptio" rows="2">
+							</textarea>
+						</div>
+						<input class="btn btn-success btn-block" type="submit" value="Save Task">
+
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
  <?php include("includes/footer.php"); ?>
